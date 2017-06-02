44d6a9ee825b597a1baf52a5604759143095af54
GrapeSoftCN <jk282@163.com>
2017-06-02 19:02:57 +0800

    增加权限，优化

--- src/main/java/interfaceApplication/MCompany.java
+++ src/main/java/interfaceApplication/MCompany.java
@@ -26,54 +26,50 @@ public class MCompany {
 	}
 
 	public String AddComp(String compInfo) {
-//		String tip = execRequest
-//				._run("GrapeAuth/Auth/InsertPLV/s:" + userid, null).toString();
-//		if (!"0".equals(tip)) {
-//			return model.resultMessage(4, "");
-//		}
-		return model.resultMessage(JSONHelper.string2json(
-				model.addComp(map, JSONHelper.string2json(compInfo))));
+		// String tip = execRequest
+		// ._run("GrapeAuth/Auth/InsertPLV/s:" + userid, null).toString();
+		// if (!"0".equals(tip)) {
+		// return model.resultMessage(4, "");
+		// }
+		return model.addComp(model.AddMap(map, JSONHelper.string2json(compInfo)));
 	}
 
 	public String updateComp(String cid, String comInfo) {
-//		String uPLV = model.FindcomByID(cid).get("uplv").toString();
-//		String tip = execRequest
-//				._run("GrapeAuth/Auth/UpdatePLV/s:" + uPLV + "/s:" + userid,
-//						null)
-//				.toString();
-//		if (!"0".equals(tip)) {
-//			return model.resultMessage(4, "没有编辑权限");
-//		}
-		return model.resultMessage(
-				model.updateComp(cid, JSONHelper.string2json(comInfo)),
-				"维护单位信息修改成功");
+		// String uPLV = model.FindcomByID(cid).get("uplv").toString();
+		// String tip = execRequest
+		// ._run("GrapeAuth/Auth/UpdatePLV/s:" + uPLV + "/s:" + userid,
+		// null)
+		// .toString();
+		// if (!"0".equals(tip)) {
+		// return model.resultMessage(4, "没有编辑权限");
+		// }
+		return model.updateComp(cid, JSONHelper.string2json(comInfo));
 	}
 
 	public String DeleteComp(String cid) {
-//		String uPLV = model.FindcomByID(cid).get("uplv").toString();
-//		String tip = execRequest
-//				._run("GrapeAuth/Auth/UpdatePLV/s:" + uPLV + "/s:" + userid,
-//						null)
-//				.toString();
-//		if (!"0".equals(tip)) {
-//			return model.resultMessage(4, "没有编辑权限");
-//		}
-		return model.resultMessage(model.deleteComp(cid), "维护单位信息删除成功");
+		// String uPLV = model.FindcomByID(cid).get("uplv").toString();
+		// String tip = execRequest
+		// ._run("GrapeAuth/Auth/UpdatePLV/s:" + uPLV + "/s:" + userid,
+		// null)
+		// .toString();
+		// if (!"0".equals(tip)) {
+		// return model.resultMessage(4, "没有编辑权限");
+		// }
+		return model.deleteComp(cid);
 	}
 
 	public String SearchComp(String Info) {
-		return model.resultMessage(model.find(JSONHelper.string2json(Info)));
+		return model.find(JSONHelper.string2json(Info));
 	}
 
 	// 分页
 	public String PageMComp(int idx, int pageSize) {
-		return model.resultMessage(model.page(idx, pageSize));
+		return model.page(idx, pageSize);
 	}
 
 	// 条件分页
 	public String PageByMComp(int idx, int pageSize, String msgInfo) {
-		return model.resultMessage(
-				model.page(idx, pageSize, JSONHelper.string2json(msgInfo)));
+		return model.page(idx, pageSize, JSONHelper.string2json(msgInfo));
 	}
 
 }
--- src/main/java/model/MCompanyModel.java
+++ src/main/java/model/MCompanyModel.java
@@ -16,6 +16,7 @@ import esayhelper.DBHelper;
 import esayhelper.formHelper;
 import esayhelper.jGrapeFW_Message;
 import esayhelper.formHelper.formdef;
+import nlogger.nlogger;
 
 public class MCompanyModel {
 	private static DBHelper comp;
@@ -23,18 +24,20 @@ public class MCompanyModel {
 	private JSONObject _obj = new JSONObject();
 
 	static {
-		comp = new DBHelper(appsProxy.configValue().get("db").toString(),
-				"ManageComp");
+		comp = new DBHelper(appsProxy.configValue().get("db").toString(), "ManageComp");
 		form = comp.getChecker();
 	}
 
 	public MCompanyModel() {
 		form.putRule("companyName", formdef.notNull);
+		form.putRule("companyEmail", formdef.email);
+		form.putRule("companyMob", formdef.mobile);
 	}
 
-	private db bind(){
+	private db bind() {
 		return comp.bind(String.valueOf(appsProxy.appid()));
 	}
+
 	/**
 	 * \ 新增操作，带权限验证
 	 * 
@@ -42,86 +45,118 @@ public class MCompanyModel {
 	 * @param object
 	 * @return
 	 */
-	public String addComp(HashMap<String, Object> map, JSONObject object) {
+	public String addComp(JSONObject object) {
+		String info = "";
+		if (object == null) {
+			return resultMessage(0, "新增数据失败");
+		}
 		if (!form.checkRuleEx(object)) {
-			return resultMessage(1, ""); // 必填字段没有填
+			return resultMessage(1); // 必填字段没有填
 		}
 		if (object.containsKey("companyEmail")) {
-			form.putRule("companyEmail", formdef.email);
 			if (!form.checkRule(object)) {
-				return resultMessage(2, ""); // 邮箱格式不正确
+				return resultMessage(2); // 邮箱格式不正确
 			}
 		}
 		if (object.containsKey("companyMob")) {
-			form.putRule("companyMob", formdef.mobile);
 			if (!form.checkRule(object)) {
-				return resultMessage(3, ""); // 手机号格式不正确
+				return resultMessage(3); // 手机号格式不正确
 			}
 		}
-		object = AddMap(map, object);
-		String info = bind().data(object).insertOnce().toString();
-		return FindcomByID(info).toString();
+		info = bind().data(object).insertOnce().toString();
+		if (("").equals(info)) {
+			return resultMessage(0, "新增数据失败");
+		}
+		JSONObject obj = FindcomByID(info);
+		return resultMessage(obj);
 	}
 
-	public int updateComp(String mid, JSONObject object) {
-		return bind().eq("_id", new ObjectId(mid)).data(object).update() != null
-				? 0 : 99;
+	public String updateComp(String mid, JSONObject object) {
+		JSONObject obj = bind().eq("_id", new ObjectId(mid)).data(object).update();
+		return obj != null ? resultMessage(0, "修改成功") : resultMessage(99);
 	}
 
-	public int deleteComp(String mid) {
-		return bind().eq("_id", new ObjectId(mid)).delete() != null ? 0 : 99;
+	public String deleteComp(String mid) {
+		JSONObject object = bind().eq("_id", new ObjectId(mid)).delete();
+		return object != null ? resultMessage(0, "删除成功") : resultMessage(99);
 	}
 
-	public int deleteCompe(String[] mids) {
+	public String deleteCompe(String[] mids) {
 		bind().or();
 		for (int i = 0; i < mids.length; i++) {
 			bind().eq("_id", new ObjectId(mids[i]));
 		}
-		return bind().deleteAll() == mids.length ? 0 : 99;
+		return bind().deleteAll() == mids.length ? resultMessage(0, "删除成功") : resultMessage(99);
 	}
 
-	public JSONArray find(JSONObject Info) {
-		for (Object object2 : Info.keySet()) {
-			if ("_id".equals(object2.toString())) {
-				bind().eq("_id", new ObjectId(Info.get("_id").toString()));
+	public String find(JSONObject Info) {
+		JSONArray array = null;
+		try {
+			if (Info != null) {
+				array = new JSONArray();
+				for (Object object2 : Info.keySet()) {
+					if ("_id".equals(object2.toString())) {
+						bind().eq("_id", new ObjectId(Info.get("_id").toString()));
+					}
+					bind().eq(object2.toString(), Info.get(object2.toString()));
+				}
+				array = bind().limit(20).select();
 			}
-			bind().eq(object2.toString(), Info.get(object2.toString()));
+		} catch (Exception e) {
+			array = null;
 		}
-		return bind().limit(20).select();
+		return resultMessage(array);
 	}
 
 	@SuppressWarnings("unchecked")
-	public JSONObject page(int idx, int pageSize) {
-		JSONArray array = bind().page(idx, pageSize);
-		JSONObject object = new JSONObject();
-		object.put("totalSize",
-				(int) Math.ceil((double) bind().count() / pageSize));
-		object.put("currentPage", idx);
-		object.put("pageSize", pageSize);
-		object.put("data", array);
-		return object;
+	public String page(int idx, int pageSize) {
+		JSONObject object = null;
+		if (idx > 0 && pageSize > 0) {
+			try {
+				object = new JSONObject();
+				JSONArray array = bind().page(idx, pageSize);
+				object.put("totalSize", (int) Math.ceil((double) bind().count() / pageSize));
+				object.put("currentPage", idx);
+				object.put("pageSize", pageSize);
+				object.put("data", array);
+			} catch (Exception e) {
+				object = null;
+			}
+		}
+		return resultMessage(object);
 	}
 
 	@SuppressWarnings("unchecked")
-	public JSONObject page(int idx, int pageSize, JSONObject Info) {
-		for (Object object2 : Info.keySet()) {
-			if ("_id".equals(object2.toString())) {
-				bind().eq("_id", new ObjectId(Info.get("_id").toString()));
+	public String page(int idx, int pageSize, JSONObject Info) {
+		JSONObject object = null;
+		if (Info != null) {
+			if (idx > 0 && pageSize > 0) {
+				object = new JSONObject();
+				try {
+					for (Object object2 : Info.keySet()) {
+						if ("_id".equals(object2.toString())) {
+							bind().eq("_id", new ObjectId(Info.get("_id").toString()));
+						}
+						bind().eq(object2.toString(), Info.get(object2.toString()));
+					}
+					JSONArray array = bind().dirty().page(idx, pageSize);
+					object = new JSONObject();
+					object.put("totalSize", (int) Math.ceil((double) bind().count() / pageSize));
+					object.put("currentPage", idx);
+					object.put("pageSize", pageSize);
+					object.put("data", array);
+				} catch (Exception e) {
+					nlogger.logout(e);
+					object = null;
+				}
 			}
-			bind().eq(object2.toString(), Info.get(object2.toString()));
 		}
-		JSONArray array = bind().dirty().page(idx, pageSize);
-		JSONObject object = new JSONObject();
-		object.put("totalSize",
-				(int) Math.ceil((double) bind().count() / pageSize));
-		object.put("currentPage", idx);
-		object.put("pageSize", pageSize);
-		object.put("data", array);
-		return object;
+		return resultMessage(object);
 	}
 
 	public JSONObject FindcomByID(String mid) {
-		return bind().eq("_id", new ObjectId(mid)).find();
+		JSONObject object = bind().eq("_id", new ObjectId(mid)).find();
+		return object != null ? object : null;
 	}
 
 	public String getID() {
@@ -138,12 +173,13 @@ public class MCompanyModel {
 	 */
 	@SuppressWarnings("unchecked")
 	public JSONObject AddMap(HashMap<String, Object> map, JSONObject object) {
+		if (object == null) {
+			return null;
+		}
 		if (map.entrySet() != null) {
-			Iterator<Entry<String, Object>> iterator = map.entrySet()
-					.iterator();
+			Iterator<Entry<String, Object>> iterator = map.entrySet().iterator();
 			while (iterator.hasNext()) {
-				Map.Entry<String, Object> entry = (Map.Entry<String, Object>) iterator
-						.next();
+				Map.Entry<String, Object> entry = (Map.Entry<String, Object>) iterator.next();
 				if (!object.containsKey(entry.getKey())) {
 					object.put(entry.getKey(), entry.getValue());
 				}
@@ -152,14 +188,24 @@ public class MCompanyModel {
 		return object;
 	}
 
+	public String resultMessage(int num) {
+		return resultMessage(num, "");
+	}
+
 	@SuppressWarnings("unchecked")
 	public String resultMessage(JSONObject object) {
+		if (object == null) {
+			object = new JSONObject();
+		}
 		_obj.put("records", object);
 		return resultMessage(0, _obj.toString());
 	}
 
 	@SuppressWarnings("unchecked")
 	public String resultMessage(JSONArray array) {
+		if (array == null) {
+			array = new JSONArray();
+		}
 		_obj.put("records", array);
 		return resultMessage(0, _obj.toString());
 	}
